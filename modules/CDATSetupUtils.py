5a97de52371eae93b49ed9fb7f6ed5138114fa9e
muryanto1 <muryanto1@llnl.gov>
2019-11-20 12:17:07 -0800

    for install from nightly, install using -c cdat/label/nightly -c conda-forge

--- modules/CDATSetupUtils.py
+++ modules/CDATSetupUtils.py
@@ -73,11 +73,9 @@ def install_nightly(workdir, conda_path, env_prefix, py_ver):
 
     conda_cmd = os.path.join(conda_path, 'conda')
     if sys.platform == 'darwin':
-        #ch1 = "-c cdat/label/nightly -c conda-forge"
-        ch1 = "-c conda-forge -c cdat/label/nightly"
+        ch1 = "-c cdat/label/nightly -c conda-forge"
     else:
-        #ch1 = "-c cdat/label/nightly -c conda-forge"
-        ch1 = "-c conda-forge -c cdat/label/nightly"
+        ch1 = "-c cdat/label/nightly -c conda-forge"
     ch2 = "-c pcmdi/label/nightly -c pcmdi"
 
     base_pkgs = "mesalib pcmdi_metrics cia easydev nbsphinx myproxyclient testsrunner coverage pytest"
